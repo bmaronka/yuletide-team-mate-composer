01d37c2de3bebb4eb005f6c9a275d438ee76ff8f
Bartek <bartek.maronka@gmail.com>
2024-02-08 10:56:53 +0100

    refactor animations to use flutter_animate lib

--- lib/presentation/screen/splash/splash_screen.dart
+++ lib/presentation/screen/splash/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:rive/rive.dart';
 import 'package:yuletide_team_mate_composer/constants/rive_animations.dart';
 import 'package:yuletide_team_mate_composer/extensions/build_context_extension.dart';
@@ -8,116 +10,106 @@ import 'package:yuletide_team_mate_composer/generated/l10n.dart';
 import 'package:yuletide_team_mate_composer/presentation/router/router.gr.dart';
 import 'package:yuletide_team_mate_composer/style/typography.dart';
 
+const _animationTime = 600;
+const _animationDelayTime = 6000;
+const _tapToContinueAnimationDelayTime = _animationDelayTime + 2 * _animationTime;
+
 @RoutePage()
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends HookWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
+  Widget build(BuildContext context) {
+    final tapToContinueAnimationController = useAnimationController();
+    final christmasAnimationController = useRef(SimpleAnimation(MerryChristmasAnimation.animation));
+    ObjectRef<SMITrigger?> christmasTrigger = useRef<SMITrigger?>(null);
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
-  late AnimationController _tapToContinueAnimationController;
-  late AnimationController _loadingAnimationController;
-  late RiveAnimationController _christmasAnimationController;
-
-  late SMITrigger _christmasTrigger;
-
-  @override
-  void initState() {
-    _christmasAnimationController = SimpleAnimation(MerryChristmasAnimation.animation);
-    _loadingAnimationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 700),
-    );
-    _tapToContinueAnimationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 700),
+    useEffect(
+      () {
+        christmasAnimationController.value.isActive = true;
+        return christmasAnimationController.value.dispose;
+      },
+      [],
     );
 
-    _christmasAnimationController.isActive = true;
-    _loadingAnimationController.forward();
-    Future.delayed(const Duration(seconds: 6), () {
-      _loadingAnimationController.reverse();
-      _tapToContinueAnimationController.forward();
-    });
+    final riveAnimationInit = useCallback(
+      (Artboard artboard) {
+        StateMachineController? stateMachineController = StateMachineController.fromArtboard(
+          artboard,
+          MerryChristmasAnimation.stateMachine,
+        );
+        artboard.addController(stateMachineController!);
 
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _christmasAnimationController.dispose();
-    _loadingAnimationController.dispose();
-    _tapToContinueAnimationController.dispose();
-    super.dispose();
-  }
-
-  void _init(Artboard artboard) {
-    StateMachineController? stateMachineController = StateMachineController.fromArtboard(
-      artboard,
-      MerryChristmasAnimation.stateMachine,
+        christmasTrigger.value = stateMachineController.findInput<bool>(MerryChristmasAnimation.input1)! as SMITrigger;
+      },
+      [],
     );
-    artboard.addController(stateMachineController!);
+    final continueToNextPage = useCallback(
+      () {
+        if (tapToContinueAnimationController.isCompleted) {
+          christmasTrigger.value?.change(true);
+          tapToContinueAnimationController.reset();
+          Future.delayed(
+            const Duration(milliseconds: _animationDelayTime),
+            () => context.router.push(const HomeRoute()),
+          );
+        }
+      },
+      [],
+    );
 
-    _christmasTrigger = stateMachineController.findInput<bool>(MerryChristmasAnimation.input1)! as SMITrigger;
-  }
-
-  void _continueToNextPage() {
-    if (_tapToContinueAnimationController.isCompleted) {
-      _christmasTrigger.change(true);
-      _tapToContinueAnimationController.reset();
-      Future.delayed(
-        const Duration(seconds: 5),
-        () => context.router.push(const HomeRoute()),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        body: GestureDetector(
-          onTap: _continueToNextPage,
-          behavior: HitTestBehavior.translucent,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              IgnorePointer(
-                child: Assets.animations.merryChristmas.rive(
-                  artboard: MerryChristmasAnimation.artboard,
-                  fit: BoxFit.fill,
-                  controllers: [_christmasAnimationController],
-                  onInit: _init,
-                ),
+    return Scaffold(
+      body: GestureDetector(
+        onTap: continueToNextPage,
+        behavior: HitTestBehavior.translucent,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            IgnorePointer(
+              child: Assets.animations.merryChristmas.rive(
+                artboard: MerryChristmasAnimation.artboard,
+                fit: BoxFit.fill,
+                controllers: [christmasAnimationController.value],
+                onInit: riveAnimationInit,
               ),
-              Positioned(
-                top: 50,
-                left: 0,
-                right: 0,
-                child: FadeTransition(
-                  opacity: _loadingAnimationController,
-                  child: Text(
-                    Strings.of(context).loading,
-                    style: StandardTypography.header1.copyWith(color: context.getColors().primary),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 5,
-                left: 20,
-                right: 20,
-                child: FadeTransition(
-                  opacity: _tapToContinueAnimationController,
-                  child: Text(
-                    Strings.of(context).tapToContinue,
-                    style: StandardTypography.header1.copyWith(color: context.getColors().primary),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+            Positioned(
+              top: 50,
+              left: 0,
+              right: 0,
+              child: Text(
+                Strings.of(context).loading,
+                style: StandardTypography.header1.copyWith(color: context.getColors().primary),
+                textAlign: TextAlign.center,
+              )
+                  .animate(
+                    onComplete: (controller) => Future.delayed(
+                      const Duration(milliseconds: _animationDelayTime),
+                      controller.reverse,
+                    ),
+                  )
+                  .fadeIn(duration: const Duration(milliseconds: _animationTime))
+                  .scale(),
+            ),
+            Positioned(
+              bottom: 30,
+              left: 20,
+              right: 20,
+              child: Text(
+                Strings.of(context).tapToContinue,
+                style: StandardTypography.header1.copyWith(color: context.getColors().primary),
+                textAlign: TextAlign.center,
+              )
+                  .animate(
+                    controller: tapToContinueAnimationController,
+                    delay: const Duration(milliseconds: _tapToContinueAnimationDelayTime),
+                  )
+                  .fadeIn(duration: const Duration(milliseconds: _animationTime))
+                  .scale(),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
