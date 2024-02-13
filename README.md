45c6acaa1963602b51be314573abebc4147e15fb
Bartek <bartek.maronka@gmail.com>
2024-02-13 14:35:06 +0100

    minor fixes

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Yuletide Team Mate Composer
 
-Rive
+Rive & native animations
+Internationalizing
+
+# Tools
+
 flutter_animate
 flutter_hooks
+injectable
+get_it: 7.6.4
+flutter_bloc: 8.1.3
+flutter_hooks: 0.20.4
+auto_route: 7.8.4
--- lib/presentation/screen/home/widgets/animated_drawer_icon.dart
+++ lib/presentation/screen/home/widgets/animated_drawer_icon.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:yuletide_team_mate_composer/extensions/build_context_extension.dart';
 
 class AnimatedDrawerIcon extends StatelessWidget {
@@ -18,16 +19,16 @@ class AnimatedDrawerIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Positioned(
-        top: 40,
-        left: 20,
+        top: 36.h,
+        left: 4.w,
         child: Animate(controller: controller, autoPlay: false).custom(
           duration: animationDuration,
           curve: Curves.easeIn,
           builder: (_, value, __) => GestureDetector(
             onTap: onTap,
             child: Container(
-              margin: const EdgeInsets.all(12),
-              padding: const EdgeInsets.all(8),
+              margin: EdgeInsets.all(12.w),
+              padding: EdgeInsets.all(8.w),
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 color: Color.lerp(context.getColors().olive, context.getColors().white, value),
